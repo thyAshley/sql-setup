2bcab5d980572583b905d6ab670ae5d4079731ef
thyashley <thy.ashley@gmail.com>
2022-05-07 19:36:52 +0800

    subqueries

--- postgres-course/orders/03-subquery.sql
+++ postgres-course/orders/03-subquery.sql
@@ -6,3 +6,11 @@ where price > (
 
 -- sub query in select statement
 select name, price, price / (select max(price) from phones) as price_ratio from phones
+
+-- sub query in from statement
+select MAX(p.avg_price) 
+from (
+  select AVG(price) as avg_price 
+  from phones
+  group by manufacturer
+)
